24c6c270d71f99ea98e0b9da91dec9ed2f63a135
albaizq <albaizq13@gmail.com>
2016-05-09 11:56:37 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -1,8 +1,8 @@
 from github import Github
 import os
 
-username = albaizq  
-password = albita1993
+username = "albaizq"  
+password = "albita1993"
 g = Github(username, password)
 
 for repo in g.get_user().get_repos():

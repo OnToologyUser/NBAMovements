62896ae98e6e1004c8f1806ece612ed7ec1f04b8
albaizq <albaizq13@gmail.com>
2016-05-09 12:12:36 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -4,6 +4,11 @@ import os
 username = "albaizq"  
 password = "albita1993"
 g = Github(username, password)
+ontoology_home_name = 'OnToology'
+
+
 
 for repo in g.get_user().get_repos():
     print repo.name
+    files = repo.get_git_tree(sha=sha, recursive=True).tree
+    print files

2e329204880b01f3fea47a7751e43c8c19d5896d
albaizq <albaizq13@gmail.com>
2016-05-09 10:51:05 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -2,11 +2,7 @@ import os
 from github import Github
 
 def init_g():
-  global g
-  username = os.environ['github_username']
-  password = os.environ['github_password']
-  g = Github(username, password)
-  print g
+  print "Hola"
   
 if __name__ == "__main__":
   init_g()

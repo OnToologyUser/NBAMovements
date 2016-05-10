fcf87a1c8959ec1f324ee7f96abb914b1276d103
albaizq <albaizq13@gmail.com>
2016-05-10 13:15:30 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -16,11 +16,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 file = open('Requisito1.rq', 'r')
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-sparql.setQuery("""
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT ?label
-    WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }
-""")
+sparql.setQuery(file.read())
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 

10a92c00995c38127447e30e91dfe02460f56867
albaizq <albaizq13@gmail.com>
2016-05-10 12:27:39 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -1,13 +1,25 @@
-from github import Github
-import os
+#from github import Github
+#import os
 
-username = "albaizq"  
-password = "albita1993"
-g = Github(username, password)
-ontoology_home_name = 'OnToology'
+#username = "albaizq"  
+#password = "albita1993"
+#g = Github(username, password)
+#ontoology_home_name = 'OnToology'
 
 
 
-for repo in g.get_user().get_repos():
-    print repo.name
-    repo.create_issue('Travis CI test', repo.name)
+#for repo in g.get_user().get_repos():
+#    print repo.name
+ #   repo.create_issue('Travis CI test', repo.name)
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+file = open('Requisito1.rq', 'r')
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql.setQuery(""" + file.read() + 
+""")
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+for result in results["results"]["bindings"]:
+    print(result["label"]["value"])

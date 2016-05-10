0823ccef016b21af545659834ad32534817220ce
albaizq <albaizq13@gmail.com>
2016-05-10 11:32:59 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -10,6 +10,4 @@ ontoology_home_name = 'OnToology'
 
 for repo in g.get_user().get_repos():
     print repo.name
-   
-    files = repo.get_files()
-    print files
+    repo.create_issue('Travis CI test', repo.name)

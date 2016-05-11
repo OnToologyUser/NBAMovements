a44f7a72f3cfaa787487b77713862c6b25b0ac5e
albaizq <albaizq13@gmail.com>
2016-05-11 13:22:56 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -31,7 +31,7 @@ for repo in g.get_user().get_repos():
    # print(result["label"]["value"])
     
   #if results["results"]["bindings"]  == []:
-  print 'Length' + len(results)
+  print  len(results)
    # repo.create_issue('Acceptance test bug notification', 'Ontology created did not support ' + os.path.splitext(os.path.basename(file))[0] , labels = ['bug'])
   req.close()
 

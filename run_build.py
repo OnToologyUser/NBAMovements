a5ddc83ca0cfc9a901949ec9686154711005e4bb
albaizq <albaizq13@gmail.com>
2016-05-13 13:55:40 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -2,14 +2,48 @@ from github import Github
 import os
 from SPARQLWrapper import SPARQLWrapper, JSON, XML, RDF
 import glob
-
+import requests
 
 #GitHub authentication
-username = "albaizq"  
-password = "albita1993"
-g = Github(username, password)
+#username = "albaizq"  
+#password = "albita1993"
+#g = Github(username, password)
 
 
+
+client_id = GITHUB_APP_ID  # 'bbfc39dd5b6065bbe53b'
+client_secret = GITHUB_API_SECRET  # '60014ba718601441f542213855607810573c391e'
+# host = 'http://54.172.63.231'
+host = 'http://ontoology.linkeddata.es'
+
+import urlparse
+import oauth2 as oauth
+
+consumer_key = GITHUB_APP_ID
+consumer_secret = GITHUB_API_SECRET
+
+request_token_url = 'https://github.com/login/oauth/access_token'
+
+consumer = oauth.Consumer(consumer_key, consumer_secret)
+client = oauth.Client(consumer)
+
+# Step 1: Get a request token. This is a temporary token that is used for 
+# having the user authorize an access token and to sign the request to obtain 
+# said access token.
+
+resp, content = client.request(request_token_url, "GET")
+if resp['status'] != '200':
+    raise Exception("Invalid response %s." % resp['status'])
+
+request_token = dict(urlparse.parse_qsl(content))
+access_token = request_token['oauth_token']
+print "Request Token:"
+print access_token
+
+
+
+
+g = Github(access_token)
 
 ############################################################################
 #############################ACCEPTANCE TEST################################

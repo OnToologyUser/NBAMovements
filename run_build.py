4a0349cf7dd62caad6e4fd7ac97f8514db470a8e
albaizq <albaizq13@gmail.com>
2016-05-16 12:36:01 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -216,7 +216,7 @@ for repo in g.get_user().get_repos():
     print 'will create an oops issue'
     try:
         repo.create_issue(
-            'OOPS! Evaluation for ' + os.path.splitext(os.path.basename(ont_file))[0]., oops_issues, labels = label)
+            'OOPS! Evaluation for ' + os.path.splitext(os.path.basename(ont_file))[0], oops_issues, labels = label)
     except Exception as e:
         print 'exception when creating issue: ' + str(e)
 

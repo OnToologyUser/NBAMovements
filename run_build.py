f1113ee936cad796d080ddb89d84de4286d6dca3
albaizq <albaizq13@gmail.com>
2016-05-19 12:46:40 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -40,7 +40,7 @@ for repo in g.get_user().get_repos():
     	# repo.create_issue('Acceptance test  notification', 'The ontology created did not support the requirement with ID ' + os.path.splitext(os.path.basename(file))[0].split("_")[1] , labels = ['Acceptance test bug'])
     else:
     	for result in list_results:
-    		if not list(result.iter())[1].text in list_result_user:
+    		if not list(result.iter())[1].text in list_results_user:
     			print 'not results'
     		else:
     			print 'yes'

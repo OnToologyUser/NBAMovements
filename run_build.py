51abf1de91f20ca6d231d0079a2755eee70bedd8
albaizq <albaizq13@gmail.com>
2016-05-24 21:08:44 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -88,7 +88,7 @@ for repo in g.get_user().get_repos():
         	tag = list(result.iter())[1].tag
         	print tag
         	print list(result.iter())[1].attrib
-    	   	if not (type_res.replace(" ","").replace("\n","") in list(result.iter())[1].tag or type_res.replace(" ","").replace("\n","") in list(result.iter())[1].attrib:
+    	   	if not (type_res.replace(" ","").replace("\n","") in list(result.iter())[1].tag or type_res.replace(" ","").replace("\n","") in list(result.iter())[1].attrib):
     	   		if not ("len" or  "list" in error_list):
     	   				i += 1
     	   				s += "%d. " % (i) + 'The ontology created did not support the requirement with ID ' + os.path.splitext(os.path.basename(file))[0].split("_")[1]+'.\n'

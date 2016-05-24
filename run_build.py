22b362edd00cec8761ed90e100dfcf9bc2e2b192
albaizq <albaizq13@gmail.com>
2016-05-24 22:10:59 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -86,7 +86,9 @@ for repo in g.get_user().get_repos():
         #check if the types are the same that the user expected
         for result in list_results:
         	tag = list(result.iter())[1].tag
-        	attrib = list(result.iter())[1].attrib['datatype']
+        	attrib = list(result.iter())[1].attrib
+        	if attrib is not None:
+        		attrib = attrib.values()[0]
 		options = [tag, attrib]
 		if not any(type_res.replace(" ","").replace("\n","")  in op for op in options ):
     	   		if not ("len" or  "list" in error_list):

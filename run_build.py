fb895db4d70ecc824553111aed58d1573507f63d
albaizq <albaizq13@gmail.com>
2016-05-27 17:39:10 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -47,9 +47,7 @@ for repo in g.get_user().get_repos():
     list_e = []
     for result in list_results:
     	#if not list(result.iter())[1].attrib == "head":
-    		print result.text
-    		print result.attrib
-    		el = result.get('http://www.w3.org/2005/sparql-results#}binding')
+    		el = result.find('http://www.w3.org/2005/sparql-results#}binding')
     		print el
     		for element in el:
     			list_e.append(list(element.iter())[1].text)

5fa22dc09156f3ad920de7bcaa76a9cddc44350c
albaizq <albaizq13@gmail.com>
2016-05-27 21:30:42 +0200

    Update run_build.py

--- run_build.py
+++ run_build.py
@@ -45,7 +45,7 @@ for repo in g.get_user().get_repos():
     
     list_e = []
     for result in list_results:
-    		list_e[:] = []
+    		list_e = []
     	#if not list(result.iter())[1].attrib == "head":
     		el = result.findall('{http://www.w3.org/2005/sparql-results#}binding')
     		for element in el:
@@ -53,6 +53,8 @@ for repo in g.get_user().get_repos():
     			print list(element.iter())[1].text
     			list_e.append(list(element.iter())[1].text)
     			print list_e
+    		print 'list elements antes'
+    		print list_elements_results
     		print 'list e despues de for'
     		print list_e
     		list_elements_results.append(list_e)
